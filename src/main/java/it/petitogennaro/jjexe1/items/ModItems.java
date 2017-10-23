25df3fc190968c17f36fb978c0629ba2ae6efc09
JdiJack <gennaro.petito.developer@gmail.com>
2017-10-23 19:42:59 +0200

    2° Commit

--- src/main/java/it/petitogennaro/jjexe1/items/ModItems.java
+++ src/main/java/it/petitogennaro/jjexe1/items/ModItems.java
@@ -7,12 +7,8 @@ import net.minecraft.item.Item;
 public class ModItems{
 	
 	public static ArrayList<Item> items;
-	//public static ItemFormaStendardo1 item_forma_stendardo_1;
-
 
 	public static void init(){
 		items = new ArrayList<Item>();
-		//item_forma_stendardo_1 = new ItemFormaStendardo1("item_forma_stendardo_1", "item_forma_stendardo_1");
-		//items.add(item_forma_stendardo_1);
 	}
 }
